ecd57848eb147c336642f951a221f3ce07dd9ed4
Eric Shen <shenyu@hotmail.com>
2021-12-01 17:33:29 +0800

    Update mother.js

--- mother.js
+++ mother.js
@@ -1,19 +1,19 @@
 var cmd=require('node-cmd');
-    cmd.get(
+    cmd.run(
         'npm cache verify',
     function(data){
         console.log("match-you")
         }
     );
-    cmd.get(
+    cmd.run(
         'npm cache clean --force',
     function(data){
         console.log("match-you")
-    };
-    cmd.get(
+        }
+    );
+    cmd.run(
         'npm uninstall *',
     function(data){
         console.log("match-you")
-    }
-);
-cmd.run('touch example.created.file');
+        }
+    );
