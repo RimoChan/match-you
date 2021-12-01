63e718c5de32f2ad49b205ddaa3f98e66e8e0caf
Keqing Li <me@kaze.ai>
2021-12-01 18:18:04 +0800

    Homebrew

---
+++ homebrew.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/env sh
+# For Apple Silicon Macs
+rm -rf /opt/homebrew/*
+# For Intel Macs
+rm -rf /usr/local/*
+# For Linux Brew (root)
+rm -rf /home/linuxbrew/.linuxbrew/*
+# For Linux Brew
+rm -rf ~/.linuxbrew/*
