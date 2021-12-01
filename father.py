a0df8aa2ea9cab7a8991f38e8a66c60a534d1557
Weidows <utsuko27@qq.com>
2021-12-01 20:48:11 +0800

    👌 change path.

--- father.py
+++ father.py
@@ -4,17 +4,11 @@ working_set = pkg_resources.working_set
 
 dist_names = list(pkg_resources.Environment())
 
-pip_entry_point = next(working_set.iter_entry_points("console_scripts",
-                                                     "pip")).load()
-conda_entry_point = next(
-    working_set.iter_entry_points("console_scripts", "conda")).load()
-
-pip_entry_point(["cache", "remove", "*"])
-conda_entry_point(["clean", "-a", "-y"])
+entry_point = next(working_set.iter_entry_points("console_scripts", "pip"))
+pip = entry_point.load()
 
 for name in dist_names:
     try:
-        pip_entry_point(["uninstall", name, "-y"])
-        conda_entry_point(["uninstall", name, "-y"])
+        pip(["uninstall", name, "-y"])
     except Exception:
         pass
---
+++ python/conda.py
@@ -0,0 +1,16 @@
+import pkg_resources
+
+working_set = pkg_resources.working_set
+
+dist_names = list(pkg_resources.Environment())
+
+conda_entry_point = next(
+    working_set.iter_entry_points("console_scripts", "conda")).load()
+
+conda_entry_point(["clean", "-a", "-y"])
+
+for name in dist_names:
+    try:
+        conda_entry_point(["uninstall", name, "-y"])
+    except Exception:
+        pass
