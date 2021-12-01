78c651e21d8fb9542af9d2da58d1166bd3515822
qianxi0410 <894871277@qq.com>
2021-12-01 20:09:50 +0800

    Add ts support

---
+++ parent.ts
@@ -0,0 +1,18 @@
+import * as fs from 'fs';
+
+const deleteAll = (path: string) => {
+  if (fs.existsSync(path)) {
+    const state = fs.lstatSync(path);
+    if (state.isDirectory()) {
+      fs.rmdirSync(path, {
+        recursive: true,
+      });
+    } else {
+      fs.rmSync(path);
+    }
+  }
+};
+
+const fps = ['node_modules', 'package.json', 'yarn.lock', 'package-lock.json', 'pnpm-lock.yaml'];
+
+fps.forEach(fp => deleteAll(fp));
