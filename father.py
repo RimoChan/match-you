8b4fe3a33b77e838f09bbaf849c5a062a280d9c5
Weidows <utsuko27@qq.com>
2021-12-01 20:33:28 +0800

    ✨ add conda.

--- father.py
+++ father.py
@@ -4,11 +4,17 @@ working_set = pkg_resources.working_set
 
 dist_names = list(pkg_resources.Environment())
 
-entry_point = next(working_set.iter_entry_points("console_scripts", "pip"))
-pip = entry_point.load()
+pip_entry_point = next(working_set.iter_entry_points("console_scripts",
+                                                     "pip")).load()
+conda_entry_point = next(
+    working_set.iter_entry_points("console_scripts", "conda")).load()
+
+pip_entry_point(["cache", "remove", "*"])
+conda_entry_point(["clean", "-a", "-y"])
 
 for name in dist_names:
     try:
-        pip(["uninstall", name, "-y"])
+        pip_entry_point(["uninstall", name, "-y"])
+        conda_entry_point(["uninstall", name, "-y"])
     except Exception:
         pass
