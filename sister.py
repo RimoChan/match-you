5394ad8b0278239066dc489aedf3b5c26bab1263
LaoshuBaby <keaitianxinmail@qq.com>
2021-12-01 20:18:21 +0800

    Create sister.py

---
+++ sister.py
@@ -0,0 +1,15 @@
+# 本文件用于清除OpenCV环境
+
+import os
+
+# 清除Python用的
+os.system("pip uninstall -y opencv-python")
+
+# 清除Visual Studio用的
+PATH=os.environ['PATH'].split(";")
+opencv_path = []
+for i in PATH:
+    if "opencv" in i:
+        opencv_path.append(i)
+for i in opencv_path:
+    os.system("rmdir /s /q " + i)
