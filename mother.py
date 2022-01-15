f16bf503ea4472ff05eeff5eb81caee0ea2d1cec
Ginta <775650117@qq.com>
2022-01-15 11:47:37 +0800

    把pip的cache也删了，要保证不留活口

--- mother.py
+++ mother.py
@@ -1,11 +1,16 @@
 import subprocess
 import os
+import shutil
 
 process = subprocess.run('pip list', shell=True, capture_output=True)
 
 for line in process.stdout.decode().strip().split('\n')[2:]:
     name, version = line.strip().split()
     subprocess.run(f'pip uninstall {name} -y')
+
+process = subprocess.run('pip cache dir', shell=True, capture_output=True)
+cache_dir = process.stdout.decode().strip()
+shutil.rmtree(cache_dir)
 
 print("Done.")
 os.remove(__file__)
