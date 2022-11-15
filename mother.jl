af00c58082e020ec7a8bcbb594bcffc4e7ccac71
mizu-bai <shiragawa4519@outlook.com>
2022-11-15 21:00:11 +0800

    Update mother.jl

--- mother.jl
+++ mother.jl
@@ -1,3 +1,3 @@
 using Pkg
 
-Pkg.dependencies() |> values |> collect |> x -> filter(p -> p.is_direct_dep, x) |> values .|>x -> x.name |> Pkg.rm
+Pkg.dependencies() |> values |> collect |> x -> filter(p -> p.is_direct_dep, x) |> values .|> x -> x.name |> Pkg.rm
